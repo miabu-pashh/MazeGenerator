3db7931b66d08229f92c9b149a67b651b12a2ee7
maibu pashh <71766168+miabu-pashh@users.noreply.github.com>
2023-04-01 03:11:22 +0000

    demo1

--- Graph.java
+++ Graph.java
@@ -3,77 +3,43 @@ import java.util.*;
 import java.util.HashMap;
 public class Graph{
 
-    private int V; // No. of vertices
-	private LinkedList<Integer> adj[]; 
-    Graph(int v)
-	{
-		V = v;
-		adj = new LinkedList[v];
-		for (int i = 0; i < v; ++i)
-			adj[i] = new LinkedList<Integer>();
-	}
-    // Function to add an edge into the graph
+    private int numNodes;
+    ArrayList<ArrayList<Integer>> graph;
 
-	void addEdge(int v, int w) { adj[v].add(w); }
-	// The function to do DFS traversal.
-    // It uses recursive
-    // DFSUtil()
-    void DFS(int v)
-    {
-        // Mark all the vertices as
-        // not visited(set as
-        // false by default in java)
-        boolean visited[] = new boolean[V];
- 
-        // Call the recursive helper
-        // function to print DFS
-        // traversal
-        DFSUtil(v, visited);
-    }
- 
-	void DFSUtil(int v, boolean visited[])
-    {
-        // Mark the current node as visited and print it
-        visited[v] = true;
-        System.out.print(v + " ");
- 
-        // Recur for all the vertices adjacent to this
-        // vertex
-        Iterator<Integer> i = adj[v].listIterator();
-        while (i.hasNext()) {
-            int n = i.next();
-            if (!visited[n])
-                DFSUtil(n, visited);
+    public Graph(int numNodes){
+        this.numNodes = numNodes;
+        this.graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < numNodes; i++)
+        {
+            this.graph.add(new ArrayList<Integer>());
         }
     }
-	
-    public static void main(String args[])
-	{
-		Graph g = new Graph(5);
 
-		g.addEdge(0,1);
-		g.addEdge(0,2);
-		g.addEdge(2,3);
-		g.addEdge(1,3);
-		g.addEdge(3,4);
-		g.addEdge(4,1);
+    public void addEdge(int u, int v){
+        boolean skip = false;
+        for (Integer node: this.graph.get(u)){
+            if (node == v){
+                skip = true;
+            }
+        }
+        if (!skip){
+            this.graph.get(u).add(v);
+        }
+    }
 
-		// for depth first Search
-		// Graph g = new Graph(4);
- 
-        // g.addEdge(0, 1);
-        // g.addEdge(0, 2);
-        // g.addEdge(1, 2);
-        // g.addEdge(2, 0);
-        // g.addEdge(2, 3);
-        // g.addEdge(3, 3);
+    public void removeEdge(int u, int v){
+        if (this.graph.get(u).contains(v)){
+            for (int i = 0; i < this.graph.get(u).size(); i++)
+            {
+                if (this.graph.get(u).get(i) == v){
+                    this.graph.get(u).remove(i);
+                }
+            }
+        }
+    }
 
-		System.out.println(
-            "Following is Depth First Traversal "
-            + "(starting from vertex 2)");
- 
-        // Function call
-        g.DFS(2);
+    public boolean hasEdge(int u, int v){
+        return this.graph.get(u).contains(v);
     }
 
 }
